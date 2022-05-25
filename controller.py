a8375db68b9dbac20f0238513b8cfb4261c9ae91
elifcansuyildiz <cansu96yildiz@gmail.com>
2022-05-25 03:33:51 +0200

    defined functionalities of reset and save buttons

--- controller.py
+++ controller.py
@@ -4,16 +4,23 @@ from PySide6.QtWidgets import QApplication, QTabWidget, QGraphicsScene, QFileDia
 
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtCore import Slot, Qt, QDir
-from PySide6.QtGui import QPixmap, QIcon, QImageReader, QGuiApplication
+from PySide6.QtGui import QPixmap, QIcon, QImageReader, QGuiApplication, QPainter
+
+import imageio
+import numpy as np
+
 
 class MyApplication():
     def __init__(self):
         loader = QUiLoader()
         self.window = loader.load("mainwindow.ui", None)
-        self.mainwindow_setup(self.window)
+        self.mainwindow_setup()
         self.window.show()
 
-    def mainwindow_setup(self, w):
+        self.image = None
+
+    def mainwindow_setup(self):
+        w = self.window
         w.setWindowTitle("Image Processing")
         w.resize(1000,800)
 
@@ -29,12 +36,27 @@ class MyApplication():
         w.treeWidget.itemClicked.connect(self.item_clicked_event)
         w.treeWidget.expandAll()
 
+        self.disable_buttons([w.save_button, w.reset_button, w.undo_button,
+                              w.fisheye_apply_button, w.swirl_apply_button,
+                              w.waves_apply_button, w.cylinder_apply_button,
+                              w.radial_apply_button, w.persmap_apply_button,
+                              w.customeffect_apply_button])
+
         pixmap = QPixmap("star_white.png")
         w.icon_label.setScaledContents(True)
         w.icon_label.setPixmap(pixmap)
 
+    def disable_buttons(self, buttons):
+        for button in buttons:
+            button.setEnabled(False)
+
+    def enable_buttons(self, buttons):
+        for button in buttons:
+            button.setEnabled(True)
+
     @Slot()
     def load_button_event(self):
+        w = self.window
         self.image_file_name = QFileDialog.getOpenFileName(self.window, "Open Image", ".", "Image Files (*.png *.jpg *.bmp)")
         reader = QImageReader(self.image_file_name[0])
         reader.setAutoTransform(True)
@@ -49,14 +71,35 @@ class MyApplication():
         self.window.graphicsView.setScene(self.scene)
         item = self.window.graphicsView.items()
         self.window.graphicsView.fitInView(item[0],Qt.KeepAspectRatio)
-        print("loaded")
+
+        self.image = self.image_read(self.image_file_name[0])
+
+        # enable the buttons that were disabled in the beginning
+        self.enable_buttons([w.save_button, w.reset_button,
+                              w.fisheye_apply_button, w.swirl_apply_button,
+                              w.waves_apply_button, w.cylinder_apply_button,
+                              w.radial_apply_button, w.persmap_apply_button,
+                              w.customeffect_apply_button])
 
     @Slot()
     def save_button_event(self):
-        print("saved")
+        file_name_to_save = QFileDialog.getSaveFileName(self.window, "Open Image", ".", "Image Files (*.png *.jpg *.bmp)")[0]
+
+        extension_list = ["png", "jpg"]
+        if any(substring in file_name_to_save for substring in extension_list) == False:
+            file_name_to_save = file_name_to_save + ".jpg"
+
+        self.image_write(self.image, file_name_to_save)
 
     @Slot()
     def reset_button_event(self):
+        self.window.graphicsView.setScene(None)
+        self.image = None
+        self.disable_buttons([self.window.save_button, self.window.reset_button, self.window.undo_button,
+                              self.window.fisheye_apply_button, self.window.swirl_apply_button,
+                              self.window.waves_apply_button, self.window.cylinder_apply_button,
+                              self.window.radial_apply_button, self.window.persmap_apply_button,
+                              self.window.customeffect_apply_button])
         print("reseted")
 
     @Slot()
@@ -91,11 +134,25 @@ class MyApplication():
         elif item_name == "About":
             self.window.tabWidget.setCurrentIndex(0)
 
+    def image_read(self, file_name, pilmode='RGB', arrtype=np.float):
+        """
+        pilmode: str
+            for luminance / intesity images use 'L'
+            for RGB color images use 'RGB'
+        arrtype: numpy dtype
+            use np.float, np.uint8, ...
+        """
+        return imageio.imread(file_name, pilmode=pilmode).astype(arrtype)
+
+    def image_write(self, image, file_name, arrtype=np.uint8):
+        imageio.imwrite(file_name, np.array(image).astype(arrtype))
+
+
 if __name__ == "__main__":
 
     app = QtWidgets.QApplication(sys.argv)
 
-    ma = MyApplication()
+    my_app = MyApplication()
 
     with open("style.qss", "r") as f:
         _style = f.read()
